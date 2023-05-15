3e9e03e0ce776240350359229643745a5f905ebd
BahiaPorcupine <bahia-porcupine@proton.me>
2023-05-15 15:42:57 +0200

    It's getting better all the time

---
+++ public/assets/js/color.js
@@ -0,0 +1,132 @@
+'use strict';
+
+const hsvToRgb = (h, s, v) => {
+    let r, g, b;
+
+    let i = Math.floor(h * 6);
+    let f = h * 6 - i;
+    let p = v * (1 - s);
+    let q = v * (1 - f * s);
+    let t = v * (1 - (1 - f) * s);
+
+    switch (i % 6) {
+        case 0:
+            (r = v), (g = t), (b = p);
+            break;
+        case 1:
+            (r = q), (g = v), (b = p);
+            break;
+        case 2:
+            (r = p), (g = v), (b = t);
+            break;
+        case 3:
+            (r = p), (g = q), (b = v);
+            break;
+        case 4:
+            (r = t), (g = p), (b = v);
+            break;
+        case 5:
+            (r = v), (g = p), (b = q);
+            break;
+    }
+    return [r * 255, g * 255, b * 255];
+}
+
+const rgbToHsv = (r, g, b) => {
+    (r /= 255), (g /= 255), (b /= 255);
+
+    let max = Math.max(r, g, b),
+        min = Math.min(r, g, b);
+    let h,
+        s,
+        v = max;
+
+    let d = max - min;
+    s = max == 0 ? 0 : d / max;
+
+    if (max == min) {
+        h = 0;
+    } else {
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
+        }
+
+        h /= 6;
+    }
+    return [h, s, v];
+}
+
+const hslToRgb = (h, s, l) => {
+    let r, g, b;
+
+    if (s == 0) {
+        r = g = b = l; // achromatic
+    } else {
+        function hue2rgb(p, q, t) {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1 / 6) return p + (q - p) * 6 * t;
+            if (t < 1 / 2) return q;
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+            return p;
+        }
+
+        let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        let p = 2 * l - q;
+
+        r = hue2rgb(p, q, h + 1 / 3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1 / 3);
+    }
+    return [r * 255, g * 255, b * 255];
+}
+
+const rgbToHsl = (r, g, b) => {
+    (r /= 255), (g /= 255), (b /= 255);
+
+    let max = Math.max(r, g, b),
+        min = Math.min(r, g, b);
+    let h,
+        s,
+        l = (max + min) / 2;
+
+    if (max == min) {
+        h = s = 0; // achromatic
+    } else {
+        let d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
+        }
+
+        h /= 6;
+    }
+    return [h, s, l];
+}
+
+const decimalToHexString = number => {
+    if (number < 0) number = 0xffffffff + number + 1;
+    let char = number.toString(16).toUpperCase();
+    if (char.length == 1 && parseInt(char, 16) > 15) char += char;
+    else if (char.length == 1 && parseInt(char, 16) <= 15) char = '0' + char;
+    return char;
+}
+
+export {hsvToRgb, rgbToHsv, hslToRgb, rgbToHsl, decimalToHexString};
--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import dom from './dom.js';
+import { hsvToRgb, rgbToHsv, hslToRgb, rgbToHsl, decimalToHexString } from './color.js';
+
 
 let input = dom.$('#color-selector');
 let select = dom.$('#variations');
@@ -187,7 +189,8 @@ class Palette {
 }
 const createPalette = (title, parent, color, harmony, variations) => {
     let container = document.createElement('div');
-    container.classList.add('palette-container');
+    container.classList.add('hidden');
+    container.setAttribute('id', `palette-container-${harmony}`);
 
     let heading = document.createElement('h2');
     heading.classList.add('palette-title');
@@ -196,11 +199,13 @@ const createPalette = (title, parent, color, harmony, variations) => {
     container.appendChild(heading);
 
     let paletteCanvas = document.createElement('canvas');
-    let palette = new Palette(paletteCanvas, 250, color, harmony, variations);
+    let canvasSize = 240;
+    let palette = new Palette(paletteCanvas, canvasSize, color, harmony, variations);
     palette.show();
 
     let colorCodeContainer = document.createElement('div');
     colorCodeContainer.classList.add('code-container');
+
 
     palette.colors.forEach(color => {
         let colorContainer = document.createElement('div');
@@ -232,11 +237,8 @@ const createPalette = (title, parent, color, harmony, variations) => {
         hslValue.classList.add('hsl');
         color.forEach((value, index) => {
             let item = document.createElement('div');
-            if (index == 0) {
-                item.textContent = `${Math.round(value * 360)}°`;
-            } else {
-                item.textContent = `${Math.round(value * 100)}%`;
-            }
+            if (index == 0) item.textContent = `${Math.round(value * 360)}°`;
+            else item.textContent = `${Math.round(value * 100)}%`;
             hslValue.appendChild(item);
         });
 
@@ -255,181 +257,34 @@ const createPalette = (title, parent, color, harmony, variations) => {
 
     container.appendChild(paletteCanvas);
     container.appendChild(colorCodeContainer);
-
     parent.appendChild(container);
 }
 
 const genPalette = (titles, modes, primaryColor, variations) => {
-    titles.forEach((title, index) => {
-        createPalette(title, parent, primaryColor, modes[index], variations);
-    });
+    titles.forEach((title, index) => createPalette(title, parent, primaryColor, modes[index], variations));
 }
-
 
 const parseRgb = input => {
     let colorCode = input;
     colorCode = colorCode
-    .split('')
-    .splice(1)
-    .join('');
+        .split('')
+        .splice(1)
+        .join('');
     let rgb = [];
-    for (let i = 0; i < colorCode.length; i += 2) {
-        rgb.push(parseInt(colorCode[i] + colorCode[i + 1], 16));
-    }
+    for (let i = 0; i < colorCode.length; i += 2) rgb.push(parseInt(colorCode[i] + colorCode[i + 1], 16));
     return rgb;
-}
-
-const hsvToRgb = (h, s, v)=> {
-    var r, g, b;
-
-    var i = Math.floor(h * 6);
-    var f = h * 6 - i;
-    var p = v * (1 - s);
-    var q = v * (1 - f * s);
-    var t = v * (1 - (1 - f) * s);
-
-    switch (i % 6) {
-        case 0:
-            (r = v), (g = t), (b = p);
-            break;
-        case 1:
-            (r = q), (g = v), (b = p);
-            break;
-        case 2:
-            (r = p), (g = v), (b = t);
-            break;
-        case 3:
-            (r = p), (g = q), (b = v);
-            break;
-        case 4:
-            (r = t), (g = p), (b = v);
-            break;
-        case 5:
-            (r = v), (g = p), (b = q);
-            break;
-    }
-    return [r * 255, g * 255, b * 255];
-}
-
-const rgbToHsv = (r, g, b) => {
-    (r /= 255), (g /= 255), (b /= 255);
-
-    var max = Math.max(r, g, b),
-        min = Math.min(r, g, b);
-    var h,
-        s,
-        v = max;
-
-    var d = max - min;
-    s = max == 0 ? 0 : d / max;
-
-    if (max == min) {
-        h = 0; // achromatic
-    } else {
-        switch (max) {
-            case r:
-                h = (g - b) / d + (g < b ? 6 : 0);
-                break;
-            case g:
-                h = (b - r) / d + 2;
-                break;
-            case b:
-                h = (r - g) / d + 4;
-                break;
-        }
-
-        h /= 6;
-    }
-    return [h, s, v];
-}
-
-const hslToRgb = (h, s, l) => {
-    var r, g, b;
-
-    if (s == 0) {
-        r = g = b = l; // achromatic
-    } else {
-        function hue2rgb(p, q, t) {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1 / 6) return p + (q - p) * 6 * t;
-            if (t < 1 / 2) return q;
-            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-        }
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-
-        r = hue2rgb(p, q, h + 1 / 3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1 / 3);
-    }
-    return [r * 255, g * 255, b * 255];
-}
-
-const rgbToHsl = (r, g, b) => {
-    (r /= 255), (g /= 255), (b /= 255);
-
-    var max = Math.max(r, g, b),
-        min = Math.min(r, g, b);
-    var h,
-        s,
-        l = (max + min) / 2;
-
-    if (max == min) {
-        h = s = 0; // achromatic
-    } else {
-        var d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-
-        switch (max) {
-            case r:
-                h = (g - b) / d + (g < b ? 6 : 0);
-                break;
-            case g:
-                h = (b - r) / d + 2;
-                break;
-            case b:
-                h = (r - g) / d + 4;
-                break;
-        }
-
-        h /= 6;
-    }
-    return [h, s, l];
-}
-
-const decimalToHexString = number => {
-    if (number < 0) {
-        number = 0xffffffff + number + 1;
-    }
-    
-    let char = number.toString(16).toUpperCase();
-    
-    if (char.length == 1 && parseInt(char, 16) > 15) {
-        char += char;
-    } else if (char.length == 1 && parseInt(char, 16) <= 15) {
-        char = '0' + char;
-    }
-    return char;
 }
 
 genPalette(titles, modes, parseRgb(input.value), select.value);
 
 select.addEventListener('change', () => {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
+    while (parent.firstChild) parent.removeChild(parent.firstChild);
     let rgb = parseRgb(input.value);
     genPalette(titles, modes, rgb, select.value);
 });
 
 input.addEventListener('change', () => {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
+    while (parent.firstChild) parent.removeChild(parent.firstChild);
     let rgb = parseRgb(input.value);
-
     genPalette(titles, modes, rgb, select.value);
 });
